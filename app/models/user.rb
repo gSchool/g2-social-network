9b5a50386489d511f0453437ad77f6b4de3bcd49
Paul Wenig <pwenig@gmail.com>
2014-05-15 14:09:23 -0600

    [#71278150] User cannot register with email blank
    Signed-off-by: Ellie Schneiders <ems2141@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, presence: {message: "cannot be blank"}
   validates :last_name, presence: {message: "cannot be blank"}
+  validates :email, presence: {message: "cannot be blank"}
 
 end
