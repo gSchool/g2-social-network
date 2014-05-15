f68e0b1e198e23cad69b244e004b29ed5986e046
Paul Wenig <pwenig@gmail.com>
2014-05-15 13:34:30 -0600

    [#71415330] User can register if password and password conf are the same
    Signed-off-by: Ellie Schneiders <ems2141@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user.first_name = params[:first_name]
-    @user.last_name = params[:last_name]
-    @user.email = params[:email]
-    @user.password = params[:password]
-    @user.save
-    redirect_to '/users'
+    @user.first_name = params[:user][:first_name]
+    @user.last_name = params[:user][:last_name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.save
+      redirect_to '/users'
+    else
+      @user
+      render 'users/new'
+    end
+
   end
 end
---
+++ db/migrate/20140515160237_add_password_hash_column.rb
@@ -0,0 +1,5 @@
+class AddPasswordHashColumn < ActiveRecord::Migration
+  def change
+    add_column :users, :password_hash, :string
+  end
+end
