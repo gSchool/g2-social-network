12b15ff92d7cbe16f836c80b29730b871f62ceed
Paul Wenig <pwenig@gmail.com>
2014-05-19 17:01:25 -0600

    initializing model with a hash instead of explicitly
    Signed-off-by: Mike Kauffman <mike.a.kauffman@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user.first_name = params[:user][:first_name]
-    @user.last_name = params[:user][:last_name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    @user = User.new(
+    first_name: params[:user][:first_name],
+    last_name: params[:user][:last_name],
+    email: params[:user][:email],
+    password: params[:user][:password],
+    password_confirmation: params[:user][:password_confirmation]
+    )
     if @user.save
       session[:id] = @user.id
       flash[:register_message] = "Welcome #{@user.email}"
