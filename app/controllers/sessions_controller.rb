bd98f4853fb47c94d5c45e0eb2ad9a30da3d602d
Paul Wenig <pwenig@gmail.com>
2014-05-20 14:41:59 -0600

    [#71686034] User sees flash message when logging out
    Signed-off-by: Cory Leistikow <cory.leistikow@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def destroy
     session[:id] = nil
-    redirect_to root_path
+    redirect_to root_path, notice: "You have been logged out"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :users
   resources :friendships
 
-  delete '/logout', to: 'sessions#destroy'
+  resource :session, only: [:destroy]
 end
