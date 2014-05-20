44bd8c76abdb646bdb5cbf0b618d5ff3d42a9ea9
Mike Gehard <mike.gehard@gmail.com>
2014-05-20 10:28:19 -0600

    Allow methods defined on ApplicationHelper to be used in views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :is_logged_in?
+
   def current_user
     if is_logged_in?
       User.find(session[:id])
