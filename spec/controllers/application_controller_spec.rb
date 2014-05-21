36632fd3cc575745fcc66bc7f29a1adaef25f5c9
Mike Gehard <mike.gehard@gmail.com>
2014-05-21 16:26:08 -0600

    Don't touch the session in the test
    This is a better way to test logging in/logging out since it
    doesn't let the tests know about how you implement the functionality

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,46 +1,30 @@
 require 'spec_helper'
 
 describe ApplicationController do
-  # controller do
-  #   def index
-  #     raise ApplicationController::AccessDenied
-  #   end
-  # end
-
   describe 'current_user' do
     it 'returns nil if the user is not logged in' do
       expect(controller.current_user).to be_nil
     end
-    it 'returns the user if there is a user logged in' do
-      user = User.create!(first_name: 'Gerard',
-                       last_name: 'Cote',
-                       email: 'gerard@example.com',
-                       password: 'hello123'
-      )
-      session[:id] = user.id
+
+    it 'returns a user if they are logged in' do
+      user = User.create!(first_name: "Bob", last_name: "Smith", email: "bob@example.com", password: "password", password_confirmation: "password")
+
+      controller.log_in(user)
 
       expect(controller.current_user).to eq user
     end
   end
 
   describe "is_logged_in?" do
-    it "returns false if the id is not set in the session" do
+    it "returns false if the is not logged in" do
       expect(controller.is_logged_in?).to eq false
     end
 
-    it "returns true if the id is set in the session" do
-      session[:id] = 123
-      expect(controller.is_logged_in?).to eq true
-    end
-  end
-
-  describe 'log_in' do
-    it 'sets id in the session' do
-      user = User.new(id: 12345)
+    it "returns true if the user is logged in" do
+      user = User.create!(first_name: "Bob", last_name: "Smith", email: "bob@example.com", password: "password", password_confirmation: "password")
 
       controller.log_in(user)
-
-      expect(session[:id]).to eq 12345
+      expect(controller.is_logged_in?).to eq true
     end
   end
 end
