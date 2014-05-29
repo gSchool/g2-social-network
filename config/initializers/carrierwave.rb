b8c5daf903b03d0fe5d47f17e2a0af74f7382348
Seth Musulin <sethmusulin@gmail.com>
2014-05-29 10:44:03 -0600

    [ci skip] Adding proper ENV syntax

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ else
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => AWS_ACCESS,
-      :aws_secret_access_key => AWS_SECRET,
+      :aws_access_key_id => ENV['AWS_ACCESS'],
+      :aws_secret_access_key => ENV['AWS_SECRET'],
       #:region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
