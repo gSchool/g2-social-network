0a32664e12f7fc9a3b98cd0eec16bbd43676ccdb
Seth Musulin <sethmusulin@gmail.com>
2014-05-29 10:20:09 -0600

    Adding mailer configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,17 +32,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "https://g2-social-network-development.herokuapp.com/"}
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: "https://g2-social-network-development.herokuapp.com/"}
+  config.action_mailer.default_url_options = { host: "https://g2-social-network-staging.herokuapp.com/"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
