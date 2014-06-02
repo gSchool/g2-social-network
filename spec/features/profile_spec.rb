f6b8818b94d6a2c4872988058dd9a9c87e116486
Kaylee Edmonson <kaylee.edmonson@gmail.com>
2014-06-02 13:58:20 -0600

    [#72353142] User can click link in Nav to see all users.
    Signed-off-by: Rachel Logie <rachel.logie1@gmail.com>

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 
 feature 'User can see their details on their profiles page' do
-  scenario 'User can see their profile details and add a profile picture' do
+
+  before do
     create_user(email: 'example@example.com', password: '12345678', confirmation: true)
     visit root_path
     click_on 'Login'
     fill_in 'Email', with: 'example@example.com'
     fill_in 'Password', with: '12345678'
     click_button 'Login'
+  end
 
+  scenario 'User can see their profile details and add a profile picture' do
     click_link 'example@example.com'
     expect(page).to have_content 'Gerard Cote'
     expect(page).to have_content 'example@example.com'
@@ -17,5 +20,10 @@ feature 'User can see their details on their profiles page' do
     attach_file('Profile pic', Rails.root.join('spec/images/unicorn_cat.jpg'))
     click_on 'Upload Picture'
     expect(page).to have_css('img', visible: 'unicorn_cat.jpg')
+  end
+
+  scenario 'User can click on link in nav to see all users' do
+    click_on 'All Users'
+    expect(page).to have_content 'All Users'
   end
 end
