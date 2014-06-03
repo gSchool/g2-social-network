4e1ba01f50219da799d57d3917afffe59cff74b6
Kaylee Edmonson <kaylee.edmonson@gmail.com>
2014-06-03 09:43:46 -0600

    [#72478124] changed env variables to uppercase
    Signed-off-by: Nathanael Burt <nathanael.burt@gmail.com>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ else
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => ENV['aws_access_key_id'],
-      :aws_secret_access_key => ENV['aws_secret_access_key'],
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       #:region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
