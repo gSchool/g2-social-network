6915f6e8e057af715ba0156fc54f4c64bcaaff0c
Nathanael Burt <nathanael.burt@gmail.com>
2014-06-04 13:56:50 -0600

    remove dotenv-deployment gem
    fix travis-ci build error
    
    Signed-off-by: Rachel Logie <rachel.logie1@gmail.com>

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ else
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'] || "fake",
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] || "fake",
       #:region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
