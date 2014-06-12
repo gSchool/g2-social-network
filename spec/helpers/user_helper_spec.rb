2ed8086263f5061ecb2a0cb9eff4cd0f994dda1c
Mike Gehard <mike.gehard@gmail.com>
2014-06-12 11:51:57 -0600

    Make one line

--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -4,8 +4,7 @@ describe UserHelper do
   describe 'user profile picture to show' do
     context 'when user has a profile picture' do
       it 'returns the link to that picture' do
-        user = new_user
-        user.profile_pic = File.open(Rails.root.join('spec/images/unicorn_cat.jpg'))
+        user = new_user(profile_pic: File.open(Rails.root.join('spec/images/unicorn_cat.jpg')))
 
         expect(UserHelper.profile_pic(user).identifier).to eq "unicorn_cat.jpg"
       end
